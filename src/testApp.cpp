470bd135244864bc1ac8a652cbdacd286b06943b
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2012-04-29 14:49:23 -0300

    tracking fingers (TODOs xmlsettings)

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -2,196 +2,205 @@
 
 //--------------------------------------------------------------
 void testApp::setup(){
-    ofEnableAlphaBlending();
-    
-    
-    //ofSetDataPathRoot("data/");
-    
-	context.setup();
+	// load the default ofxOpenNI config settings
+	// see bin/data/openni/config/ofxopenni_config.xml
+	context.setupUsingXMLFile();
+	
+	// setup resources
 	depth.setup(&context);
-	user.setup(&context, true);
-	context.toggleMirror();
-    
-	string host = "localhost";
-	int	port	= 3333;
+	image.setup(&context);
+	user.setup(&context, &depth);
 	
-	ofxXmlSettings XML;
-	
-	if (XML.loadFile("config.xml")){
-		host = XML.getValue("HOST", "localhost");
-		port = XML.getValue("PORT", 3333);
-
-		thresholdDistance = XML.getValue("THRESHOLD", 400);
-        trackedUser = XML.getValue("TRACKEDUSER", 0);
-	} else 
-		cout << "config.xml file not found" << endl;
-	
-	hManager.setup(host.c_str() ,port);
-	hManager.numberUsers(MAXUSER);
-    
-    logo.loadImage("logo.png");
-    
 	ofSetWindowShape(640, 480);
+	
+	imagePixels = new unsigned char[640 * 480];
+	img.allocate(640, 480);
+	fingers.clear();
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
 	// update the open ni context and user tracker
 	context.update();
-    depth.update();
 	user.update();
 	
-    if ( user.getNumberOfTrackedUsers() > 0){
-        if (trackedUser == 0){
-            for(int i = 1; i <= MAXUSER; i++){
-                if ( i <= user.getNumberOfTrackedUsers() )
-                    trackAndCheck( i );
-                else {
-                    hManager.deactiveLeft( i );
-                    hManager.deactiveRight( i );
-                }
-            } 
-        } else {
-            trackAndCheck( trackedUser );
-        }
-    } else {
-        cleanHands( -1 ); //clean them all
-    }
-    
-	hManager.send();
+	// Extract depth map (pix) and user mask (userPix)
+	xn::DepthGenerator depthGenerator = depth.getXnDepthGenerator();
+	xn::DepthMetaData depthmetadata;
+	depthGenerator.GetMetaData(depthmetadata);
+	unsigned short* pix = (unsigned short*) depthmetadata.Data();
+	
+	xn::UserGenerator userGenerator = user.getXnUserGenerator();
+	xn::SceneMetaData sceneMetaData;
+	userGenerator.GetUserPixels(0, sceneMetaData);
+	unsigned short *userPix = (unsigned short*)sceneMetaData.Data();
+	
+	// Calculate the deepest user point
+	float deepest = 0.0;
+	for (int i =0 ; i < 640 * 480; i++) {
+		imagePixels[i] = 0;
+		
+		if(userPix[i]>0 && pix[i]>deepest) 
+			deepest = pix[i];
+	}
+	
+	// Check for Tracked Users and then check for active hands
+	for(int i = 0; i < user.getNumTrackedUsers(); ++i){
+		ofxTrackedUser* tracked = user.getTrackedUser(i);
+		
+		if((tracked != NULL) && tracked->neck.begin.x && tracked->neck.begin.y && tracked->neck.begin.x){
+			ofPoint leftHand = tracked->left_lower_arm.end;
+			ofPoint rightHand = tracked->right_lower_arm.end;
+		
+			ofPoint leftShoulder = tracked->left_shoulder.end;
+			ofPoint rightShoulder = tracked->right_shoulder.end;
+		
+			float thresholdDistance = leftHand.distance(tracked->left_lower_arm.begin);
+			
+			//------------------------------------------------------ Check Active Hands
+			int leftDist = leftShoulder.z - leftHand.z;
+			if ( leftDist >= thresholdDistance ){
+				for( int x = 0; x < 640; x++ ){
+					for( int y = 0; y < 480; y++ ){
+						int p = y * 640 + x;
+						if ((ofDist(leftHand.x, leftHand.y, x, y) < 70) && userPix[p]>0 ){
+							char depthHand = CLAMP(255-pix[(int)leftHand.y * 640 + (int)leftHand.x] / (deepest/(220)), 0, 255);
+							char depthVal= CLAMP(255-pix[p] / (deepest/(220)), 0, 255);
+							imagePixels[p] = ((depthVal*1.1) >= depthHand )? 255:0;
+						}
+					}
+				}
+			}
+			
+			int rightDist = rightShoulder.z - rightHand.z;
+			if ( rightDist >= thresholdDistance ){
+				for( int x = 0; x < 640; x++ ){
+					for( int y = 0; y < 480; y++ ){
+						int p = y * 640 + x;
+						if ((ofDist(rightHand.x, rightHand.y, x, y) < 70) && userPix[p]>0 ){
+							char depthHand = CLAMP(255-pix[(int)rightHand.y * 640 + (int)rightHand.x] / (deepest/(220)), 0, 255);
+							char depthVal= CLAMP(255-pix[p] / (deepest/(220)), 0, 255);
+							imagePixels[p] = ((depthVal*1.1) >= depthHand )? 255:0;
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	img.setFromPixels(imagePixels, 640, 480);
+	fingers = getFingerTips(img);
 }
 
-void testApp::trackAndCheck(int userNumber){
-    if ((userNumber > 0) && (userNumber < MAXUSER+1)){
-        ofxTrackedUser* tracked = user.getTrackedUser( userNumber );
-        if((tracked != NULL) && tracked->neck.begin.x && tracked->neck.begin.y && tracked->neck.begin.x){
-            ofPoint leftHand = tracked->left_lower_arm.end;
-            ofPoint rightHand = tracked->right_lower_arm.end;
+vector<ofPoint> testApp::getFingerTips( ofxCvGrayscaleImage input) {
+	CvMemStorage*	storage = cvCreateMemStorage(0);
+	CvSeq*			contours;
+	CvPoint*		PointArray;
+	
+	int*			hull;
+    int				hullsize;
+	
+	vector<ofPoint> fingerTips;
+	
+    //START TO FIND THE HULL POINTS
+    cvFindContours( input.getCvImage(), storage, &contours, sizeof(CvContour), CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0));
+	
+	// If there is a contour it«ll make it more simple
+    if (contours)
+		contours = cvApproxPoly(contours, sizeof(CvContour), storage, CV_POLY_APPROX_DP, 10, 1 );
+	
+    for( ; contours != 0; contours = contours->h_next ){
+        int count = contours->total; // This is number point in contour
 		
-            ofPoint leftShoulder = tracked->left_shoulder.end;
-            ofPoint rightShoulder = tracked->right_shoulder.end;
+        CvRect rect = cvContourBoundingRect( contours, 1);
 		
-            //thresholdDistance = leftShoulder.distance(rightShoulder)* nArmsFactor;
-            //thresholdDistance = leftHand.distance(tracked->left_lower_arm.begin)* nArmsFactor;
-        
-            if ( (leftShoulder.z - leftHand.z ) >= thresholdDistance )
-                hManager.activeLeft( userNumber , leftHand);
-            else
-                hManager.deactiveLeft( userNumber );
-		
-            if ((rightShoulder.z - rightHand.z ) >= thresholdDistance )
-                hManager.activeRight( userNumber, rightHand);
-            else 
-                hManager.deactiveRight( userNumber );
-        
-        } else {
-            hManager.deactiveLeft( userNumber );
-            hManager.deactiveRight( userNumber );
+        if ( (rect.width*rect.height) > 300 ){		// Analize the bigger contour
+            PointArray = (CvPoint*)malloc( count*sizeof(CvPoint) ); // Alloc memory for contour point set.
+            hull = (int*)malloc(sizeof(int)*count);	// Alloc memory for indices of convex hull vertices.
+			
+            cvCvtSeqToArray(contours, PointArray, CV_WHOLE_SEQ); // Get contour point set.
+			
+            // Find convex hull for curent contour.
+            cvConvexHull( PointArray,
+						 count,
+						 NULL,
+						 CV_COUNTER_CLOCKWISE,
+						 hull,
+						 &hullsize);
+			
+			int upper = 640, lower = 0;
+			for	(int j=0; j<hullsize; j++) {
+				int idx = hull[j]; // corner index
+				if (PointArray[idx].y < upper) 
+					upper = PointArray[idx].y;
+				if (PointArray[idx].y > lower) 
+					lower = PointArray[idx].y;
+			}
+			float cutoff = lower - (lower - upper) * 0.1f;
+			
+			// find interior angles of hull corners
+			for (int j=0; j<hullsize; j++) {
+				int idx = hull[j]; // corner index
+				int pdx = idx == 0 ? count - 1 : idx - 1; //  predecessor of idx
+				int sdx = idx == count - 1 ? 0 : idx + 1; // successor of idx
+				
+				cv::Point v1 = cv::Point(PointArray[sdx].x - PointArray[idx].x, PointArray[sdx].y - PointArray[idx].y);
+				cv::Point v2 = cv::Point(PointArray[pdx].x - PointArray[idx].x, PointArray[pdx].y - PointArray[idx].y);
+				
+				float angle = acos( (v1.x*v2.x + v1.y*v2.y) / (norm(v1) * norm(v2)) );
+				
+				// low interior angle + within upper 90% of region -> we got a finger
+				if (angle < 1 && PointArray[idx].y < cutoff) {
+					int u = PointArray[idx].x;
+					int v = PointArray[idx].y;
+					
+					fingerTips.push_back(ofPoint(u,v));
+				}
+			}
+			
+            // Free memory.
+            free(PointArray);
+            free(hull);
+			
         }
     }
+	
+    cvClearMemStorage( storage );
+	
+	return fingerTips;
 }
 
-void testApp::cleanHands(int exceptUser){
-    for(int i = 0; i < MAXUSER ; i++){
-        if (i != exceptUser){
-            hManager.deactiveLeft( i );
-            hManager.deactiveRight( i );
-        }
-    }
-}
+
 
 //--------------------------------------------------------------
 void testApp::draw(){
 	ofBackground(100, 100, 100);
 	ofSetColor(255);
 	
-	depth.draw();
+	//depth.draw();
+	//image.draw(645, 10, 400, 300);
+	//image.draw(0, 0, 640, 480);
+	//ofSetColor(255);
+	//img.draw(0,0,640,480);
+	
+	
+	// draw all the users automatically?
 	user.draw();
-	hManager.draw();
 	
-	ofSetColor(250);
-    ofDrawBitmapString("Threshold distance:  " + ofToString(thresholdDistance), 5,40);
+	ofEnableBlendMode(OF_BLENDMODE_ADD);
+	ofSetColor(255);
+	img.draw(0,0,640,480);
+	ofDisableBlendMode();
 
-    if(trackedUser == 0)
-        ofDrawBitmapString("Tracked User number: ALL", 5,55);
-    else
-        ofDrawBitmapString("Tracked User number: " + ofToString(trackedUser), 5,55);
-    
-    ofSetColor(250);
-	ofDrawBitmapString("Patricio Gonzalez Vivo.com",ofGetWidth()-260, ofGetHeight()-10 );
-	logo.draw(ofGetWidth()-50, ofGetHeight()-50, 47,50.6);
-	
-	ofSetColor(255,255,255);
-    ofDrawBitmapString("fps: "+ofToString(ofGetFrameRate(), 2), 10, 15);
+	for(int i = 0; i < fingers.size(); i++){
+		ofSetColor(255,0,0);
+		ofCircle(fingers[i].x, fingers[i].y,5);
+	}
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
-    switch(key){
-        case '+':
-        case '>':
-        case OF_KEY_UP:
-            thresholdDistance++;
-            break;
-		case '-':
-        case '<':
-        case OF_KEY_DOWN:
-			thresholdDistance--;
-			break;
-        
-        case ',':
-        case OF_KEY_LEFT:
-            if (trackedUser > 0)
-                trackedUser--;
-            break;
-		case '.':
-        case OF_KEY_RIGHT:
-            if (trackedUser < MAXUSER )
-                trackedUser++;
-			break;
-
-        case '0':
-            trackedUser = 0;
-            break;
-        case '1':
-            trackedUser = 1;
-            break;
-        case '2':
-            trackedUser = 2;
-            break;
-        case '3':
-            trackedUser = 3;
-            break;
-        case '4':
-            trackedUser = 4;
-            break;
-        case '5':
-            trackedUser = 5;
-            break;
-        case '6':
-            trackedUser = 6;
-            break;
-        case '7':
-            trackedUser = 7;
-            break;
-        case '8':
-            trackedUser = 8;
-            break;
-    }
-    
-    if ((trackedUser > 0) && (trackedUser <= MAXUSER))
-        cleanHands( trackedUser );
-    
-}
-
-//--------------------------------------------------------------
-void testApp::exit(){
-    ofxXmlSettings XML;
-    if (XML.loadFile("config.xml")){
-        XML.setValue("THRESHOLD", thresholdDistance);
-        XML.setValue("TRACKEDUSER", trackedUser);
-    }
-    XML.saveFile();
+	
 }
 
 //--------------------------------------------------------------
@@ -211,8 +220,6 @@ void testApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button) {	
-    if ( x > ofGetWidth()-300 && y > ofGetHeight()-30 )
-        ofLaunchBrowser("http://www.patriciogonzalezvivo.com");
 }
 
 //--------------------------------------------------------------
--- src/testApp.h
+++ src/testApp.h
@@ -1,20 +1,33 @@
 #ifndef _TEST_APP
 #define _TEST_APP
 
-#define MAXUSER 8
+// uncomment this for OF 007 compatibility
+//#define OFX_OPENNI_BETA
 
 #include "ofMain.h"
 #include "ofxOpenNI.h"
-#include "HandManager.h"
-#include "ofxXmlSettings.h"
+#include "ofxOpenCv.h"
 
 class testApp : public ofBaseApp{
 public:
 	
+	ofxOpenNIContext	context;
+	ofxDepthGenerator	depth;
+	ofxUserGenerator	user;
+	ofxImageGenerator	image;
+	
+	float thresholdDistance;
+	
+	unsigned char * imagePixels;
+	ofxCvGrayscaleImage img;
+	
+	ofxCvContourFinder 	contourFinder;
+	vector<ofPoint> fingers;
+	vector<ofPoint> getFingerTips( ofxCvGrayscaleImage input);
+	
 	void setup();
 	void update();
 	void draw();
-    void exit();
 
 	void keyPressed  (int key);
 	void keyReleased(int key);
@@ -23,19 +36,5 @@ public:
 	void mousePressed(int x, int y, int button);
 	void mouseReleased(int x, int y, int button);
 	void windowResized(int w, int h);
-    
-    void trackAndCheck( int userNumber );
-    void cleanHands( int exceptUser );
-    
-    ofxOpenNIContext	context;
-	ofxDepthGenerator	depth;
-	ofxUserGenerator	user;
-	
-    HandManager hManager;
-    ofImage logo;
-    
-    int trackedUser;
-	
-    float thresholdDistance;
 };
 #endif
